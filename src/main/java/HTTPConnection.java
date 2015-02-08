258e96e6320f8155dd7ad4893e87ceef7d4920a0
JackWFinlay <jfin404@aucklanduni.ac.nz>
2015-02-09 10:57:58 +1300

    Update HTTPConnection.java

--- src/main/java/HTTPConnection.java
+++ src/main/java/HTTPConnection.java
@@ -6,11 +6,16 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.util.Key;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scriptella.configuration.StringResource;
 import scriptella.spi.*;
+import scriptella.util.IOUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 public class HTTPConnection extends AbstractConnection {
+
     private String HOST;
     private String BODY;
     private String TYPE;
@@ -27,6 +32,38 @@ public class HTTPConnection extends AbstractConnection {
         TYPE = connectionParameters.getStringProperty("type");
         TIME_OUT = connectionParameters.getIntegerProperty("timeout");
 
+    }
+
+    public int getTIME_OUT() {
+        return TIME_OUT;
+    }
+
+    public void setTIME_OUT(int TIME_OUT) {
+        this.TIME_OUT = TIME_OUT;
+    }
+
+    public String getHOST() {
+        return HOST;
+    }
+
+    public void setHOST(String HOST) {
+        this.HOST = HOST;
+    }
+
+    public String getBODY() {
+        return BODY;
+    }
+
+    public void setBODY(String BODY) {
+        this.BODY = BODY;
+    }
+
+    public String getTYPE() {
+        return TYPE;
+    }
+
+    public void setTYPE(String TYPE) {
+        this.TYPE = TYPE;
     }
 
     @Override
@@ -54,20 +91,29 @@ public class HTTPConnection extends AbstractConnection {
         HTTPUrl url = new HTTPUrl(HOST);
 
         try {
-            if (TYPE.equals("GET")) {
-                url.fields = resource.toString();
+            if (TYPE.toUpperCase().equals("GET")) {
+                //url.fields = ((StringResource)resource).getString();
+                BufferedReader br = new BufferedReader(new StringReader(((StringResource)resource).getString()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String [] components = line.split("=");
+                    url.put(components[0],components[1]);
+                }
 
                 logger.info("Query URL: {}", url );
                 request = requestFactory.buildGetRequest(url);
             } else { //if (TYPE.equals("POST")) {
 
-                HttpContent httpContent = (HttpContent) resource;
+                HttpContent httpContent = ByteArrayContent.fromString(null, ((StringResource)resource).getString());
 
                 request = requestFactory.buildPostRequest(url, httpContent);
-                logger.info("Request response: {}", request.toString());
+                logger.info("Query URL: {}", url );
+                logger.info("Request contents: {}", request.getContent().toString());
             }
 
             httpResponse = request.execute();
+            logger.info("Status: {}, {}", httpResponse.getStatusCode(), httpResponse.getStatusMessage());
+            logger.info("Response: {}", httpResponse.parseAsString());
 
         } catch (IOException e) {
             e.printStackTrace();
---
+++ src/test/java/HTTPConnectionTest.java
@@ -0,0 +1,61 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import scriptella.configuration.StringResource;
+import scriptella.driver.script.ParametersCallbackMap;
+import scriptella.spi.ParametersCallback;
+import scriptella.spi.Resource;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class HTTPConnectionTest {
+
+    Resource resource;
+    HTTPConnection httpConnection;
+    ParametersCallback parametersCallback;
+
+    @Before
+    public void setUp() throws Exception {
+        httpConnection = new HTTPConnection();
+        httpConnection.setHOST("http://127.0.0.1:8080");
+        httpConnection.setTIME_OUT(9999);
+        parametersCallback = new ParametersCallback() {
+            @Override
+            public Object getParameter(String s) {
+                return null;
+            }
+        };
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        httpConnection = null;
+        resource = null;
+    }
+
+    @Test
+    public void testExecuteScript_GET(){
+
+        System.out.println("Test1");
+        httpConnection.setTYPE("GET");
+
+        resource = new StringResource("abc=123\ndef=456");
+
+        httpConnection.executeScript(resource, parametersCallback);
+    }
+
+    @Test
+    public void testExecuteScript_POST(){
+
+        System.out.println("Test2");
+        httpConnection.setTYPE("POST");
+
+        resource = new StringResource("test");
+
+        httpConnection.executeScript(resource,parametersCallback);
+    }
+}
